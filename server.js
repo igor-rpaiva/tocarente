51e8ffc6c544e13ecd15fa27a1acbaed2e6fbcb5
igor-rpaiva <igor_rpaiva@hotmail.com>
2026-03-23 11:39:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -62,3 +62,9 @@ io.on('connection', (socket) => {
     waitingUsers = waitingUsers.filter(u => u !== socket);
   });
 });
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log('Servidor rodando na porta', PORT);
+});
